17b2e2ee0b07de5a34f39031995b1e10d3455012
Alexander Rader <ap.rader@yahoo.com>
2018-09-26 11:50:11 +0100

    tried to add location indicator, broke it instead

--- app/src/main/java/com/example/s1611382/ilp/MainActivity.kt
+++ app/src/main/java/com/example/s1611382/ilp/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.example.s1611382.ilp
 
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener {
 
     private lateinit var mapView: MapView
+    private lateinit var map: MapboxMap
+    private lateinit var permissionManager: PermissionsManager
+    //stores current location at all times
+    private lateinit var originLocation: Location
+
+    //gives user location
+    private var locationEngine: LocationEngine? = null
+    //for UI: icon representing user location
+    private var locationLayerPlugin: LocationLayerPlugin? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +38,74 @@ class MainActivity : AppCompatActivity() {
         Mapbox.getInstance(applicationContext, getString(R.string.access_token))
         mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
+        // this makes map variable usable in the rest of the class
+        mapView.getMapAsync{mapboxMap ->
+            map = mapboxMap
+            enableLocation()
+        }
+    }
+
+    //get user's permission for location
+    private fun enableLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            initializeLocationEngine()
+            initializeLocationLayer()
+        } else {
+            permissionManager = PermissionsManager(this)
+            permissionManager.requestLocationPermissions(this)
+        }
+    }
+
+    private fun initializeLocationEngine() {
+        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
+        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine?.activate()
+
+        val lastLocation = locationEngine?.lastLocation
+        if (lastLocation != null) {
+            originLocation = lastLocation
+            setCameraPosition(lastLocation)
+        } else {
+            locationEngine?.addLocationEngineListener(this)
+        }
+    }
+
+    private fun initializeLocationLayer() {
+        locationLayerPlugin = LocationLayerPlugin(mapView, map, locationEngine)
+        //for camera tracking location
+        locationLayerPlugin?.setLocationLayerEnabled(true)
+        locationLayerPlugin?.cameraMode = CameraMode.TRACKING
+        locationLayerPlugin?.renderMode = RenderMode.NORMAL
+    }
+
+    private fun setCameraPosition(location: Location) {
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                LatLng(location.latitude, location.longitude), 14.5))
+    }
+
+    //-- from PermissionsListener --//
+    //when user denies permission
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        TODO("not implemented") //Explain why app needs location permissions
+    }
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocation()
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    //-- from LocationEngineListener --//
+    override fun onLocationChanged(location: Location?) {
+        location?.let {
+            originLocation = location
+            setCameraPosition(location)
+        }
+    }
+    override fun onConnected() {
+        locationEngine?.requestLocationUpdates()
     }
 
 
@@ -24,34 +114,28 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         mapView.onStart()
     }
-
     override fun onResume() {
         super.onResume()
         mapView.onResume()
     }
-
     override fun onPause() {
         super.onPause()
         mapView.onPause()
     }
-
     override fun onStop() {
         super.onStop()
         mapView.onStop()
     }
-
     override fun onDestroy() {
         super.onDestroy()
         mapView.onDestroy()
     }
-
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
         if (outState != null) {
             mapView.onSaveInstanceState(outState)
         }
     }
-
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
