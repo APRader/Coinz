5c7de89288696b89c00fa858f425365a4ad4a06d
Alexander Rader <ap.rader@yahoo.com>
2018-11-13 15:52:49 +0000

    Deposited coins are sent to Bank activity

--- app/src/main/java/com/example/s1611382/ilp/Bank.kt
+++ app/src/main/java/com/example/s1611382/ilp/Bank.kt
@@ -18,15 +18,18 @@ import kotlinx.android.synthetic.main.coin_text_view.view.*
 import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
-class Bank: AppCompatActivity() {
+class Bank: AppCompatActivity(), WalletFragment.OnCoinsDeposited {
     private val preferencesFile = "MyPrefsFile" // for storing preferences
     private var gold: Double? = 0.0
     private lateinit var rates: ArrayList<Coin>
     private lateinit var coinWallet: ArrayList<Coin>
+    private var coinBank : ArrayList<Coin> = arrayListOf()
 
     companion object {
         val COINWALLET = "coinWallet"
+        val COINBANK = "coinBank"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,11 +59,16 @@ class Bank: AppCompatActivity() {
             val fragment = WalletFragment()
             val args = Bundle()
             args.putParcelableArrayList(COINWALLET, coinWallet)
+            args.putParcelableArrayList(COINBANK, coinBank)
             fragment.arguments = args
-            fragmentTransaction.add(R.id.bank_id, fragment)
+            fragmentTransaction.replace(R.id.deposit_placeholder, fragment)
             fragmentTransaction.commit()
 
         }
+    }
+
+    override fun onCoinsDeposited(coinWallet: ArrayList<Coin>, coinBank: ArrayList<Coin>) {
+        print(coinWallet)
     }
 
     override fun onStart() {
@@ -72,6 +80,11 @@ class Bank: AppCompatActivity() {
         }
         val goldView: TextView = findViewById(R.id.gold_id)
         goldView.text = "Your GOLD: " + gold.toString()
+
+        val gson= Gson()
+        val json = settings.getString("lastCoinBank", "[]")
+        val type = object : TypeToken<ArrayList<Coin>>() {}.type
+        coinBank = gson.fromJson<ArrayList<Coin>>(json, type)
     }
 
     override fun onStop() {
@@ -79,6 +92,9 @@ class Bank: AppCompatActivity() {
         val settings = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE)
         val editor = settings.edit()
         editor.putString("lastGold", gold.toString())
+        val gson = Gson()
+        val json = gson.toJson(coinBank)
+        editor.putString("lastCoinBank", json)
         editor.apply()
     }
 
--- app/src/main/java/com/example/s1611382/ilp/WalletFragment.kt
+++ app/src/main/java/com/example/s1611382/ilp/WalletFragment.kt
@@ -1,5 +1,6 @@
 package com.example.s1611382.ilp
 
+import android.R.attr.fragment
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -9,19 +10,20 @@ import android.util.SparseBooleanArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.R.attr.prompt
-import android.widget.ListAdapter
-import android.widget.Toast
+import android.widget.*
 
 class WalletFragment : ListFragment() {
     private lateinit var coinWallet: ArrayList<Coin>
+    private lateinit var coinBank: ArrayList<Coin>
+
+    private lateinit var listener: OnCoinsDeposited
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         coinWallet = arguments!!.getParcelableArrayList(Bank.COINWALLET)
+        coinBank = arguments!!.getParcelableArrayList(Bank.COINBANK)
 
         val walletListAdapter = ArrayAdapter<Coin>(
                 activity,
@@ -29,62 +31,44 @@ class WalletFragment : ListFragment() {
                 coinWallet)
 
         listAdapter = walletListAdapter
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet, container, false)
-    }
+        val fragmentView = inflater.inflate(R.layout.fragment_wallet, container, false)
 
-    override fun onListItemClick(l: ListView?, v: View?, position: Int, id: Long) {
-        super.onListItemClick(l, v, position, id)
-
-        var prompt = "clicked item " + listView.getItemAtPosition(position).toString() + "\n"
-        prompt += "selected items: \n"
-        /*val count = listView.count
-        val sparseBooleanArray : SparseBooleanArray = listView.checkedItemPositions
-        for (i in 0 until count) {
-            if (sparseBooleanArray.get(i)) {
-                prompt += (listView.getItemAtPosition(i).toString() + "\n")
-            }
-        }*/
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     *
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WalletFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                WalletFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+        val depositCoinsButton: Button = fragmentView.findViewById(R.id.deposit_coins_id)
+        depositCoinsButton.setOnClickListener {
+            val count = listView.count
+            val checkedCoins: SparseBooleanArray = listView.checkedItemPositions
+            for (i in 0 until count) {
+                if (checkedCoins.get(i)) {
+                    // all coins the user has checked will get converted when button is clicked
+                    val coin = listView.getItemAtPosition(i) as Coin
+                    coinBank.add(coin)
+                    coinWallet.remove(coin)
                 }
-    }*/
+            }
+            // alert bank activity that the depositing has benn completed and pass new wallet/bank
+            listener.onCoinsDeposited(coinWallet, coinBank)
+        }
+
+        return fragmentView
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        // initalise the listener reference
+        if (context is OnCoinsDeposited) {
+            listener = context
+        } else {
+            throw ClassCastException(context.toString() + " must implement OnCoinsDeposited.")
+        }
+    }
+
+    // used to alert the bank activity that user has deposited coins
+    interface OnCoinsDeposited {
+        fun onCoinsDeposited(coinWallet: ArrayList<Coin>, coinBank: ArrayList<Coin>)
+    }
 }
